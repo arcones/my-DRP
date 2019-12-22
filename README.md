fd00750e6aebd26fb224dfec8a184a6f23b2e9e3
Marta Arcones <arcones@live.com>
2019-12-22 18:58:33 +0100

    WIP

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 > DRP: Disaster Recovery Plan
 
-Personal script to configure any Debian host to work with it.
-It install libs, applications, and configure some of them.
-For more info check directly the code of it in `setup.sh`
+Personal script to configure any Debian host to work with it. It installs libs, applications, and configure some of them. For more info check directly the code of it in `setup.sh`
 
 # Requirement
 The script should be run with `sudo`
@@ -12,13 +10,10 @@ The script should be run with `sudo`
 # Test
 There is a Dockerfile to build an image that can be used to test the script without using it directly in your host.
 
-You can build the image with:
-`docker build -f test/Dockerfile -t my-drp-test .`
+You can build the image & run the container with:
+`docker build -f test/Dockerfile -t my-drp-test . && docker run --network=host --rm my-drp-test:latest --name my-drp-test`
 
-And the run it with:
-`docker run --rm my-drp-test:latest`
-
-If the run action ends succesfully, the script is working succesfully.
+If it ends succesfully, the DRP script is working well.
 
 # Watch out
 The script does some clean up task that include removing and/or overwritting files, use it at your own risk!
--- setup.sh
+++ setup.sh
@@ -1,45 +1,46 @@
-## Author: Marta Arcones <marta.arcones@gmail.com>
-## This script is intented to prepare any Debian host to work with it
-## Debian minimal installation is recommeded
-## The remaining application & configuration will be done here
+#!/usr/bin/env bash
 
-## Basic utilities & libs
-echo "Updating package index & upgrading dependencies..."
-apt -qq update && apt -qq upgrade
-echo "Done" && echo
+# Author: Marta Arcones <marta.arcones@gmail.com>
+# This script is intented to prepare any Debian host to work with it
+# Debian minimal installation is recommeded
+# The remaining application & configuration will be done here
 
-echo "Installing basic utilities & libs..."
-apt -qq install -y wget tree curl xclip firefox vim net-tools gthumb pinta git apt-transport-https ca-certificates libssl-dev build-essential gnupg-agent software-properties-common zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev
-echo "Done" && echo
+export DEBIAN_FRONTEND=noninteractive
+export PYTHON_VERSION=3.9.0
+
+execute_command () {
+    echo "$2..." && echo
+    eval $1
+    if [ "$?" != 0 ]; then
+        echo "There was an error running \"$2\"!"
+        exit 1
+    else
+        echo "Done $2" && echo
+    fi
+}
+
+# Basic utilities & libs
+execute_command "apt update -y && apt full-upgrade -y" "updating package index & upgrading dependencies"
+
+execute_command "apt install -y wget tree curl xclip firefox vim net-tools gthumb pinta git apt-transport-https ca-certificates libssl-dev build-essential gnupg-agent software-properties-common zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev" "installing basic utilities & libs"
+
+execute_command "git --version" "testing git installation"
 
 # VIM config
-echo "Setting vim configuration..."
-cp .vimrc ~/.vimrc
-echo "Done" && echo
+execute_command "cp .vimrc ~/.vimrc" "setting vim configuration"
 
-## Docker
-echo "Removing previous installations of docker..."
-apt -qq remove -y docker docker-engine docker.io containerd runc 
-echo "Done" && echo
-    
-echo "Installing & configuring docker..."
-apt -qq install -y docker-ce docker-ce-cli containerd.io
-usermod -aG docker $USER
-echo "Done" && echo
+# Docker
+execute_command "apt remove -y docker docker-engine docker.io containerd runc" "removing previous installations of docker"
+execute_command "apt install -y docker" "installing docker"
+execute_command "docker -v" "testing docker installation"
 
-## Python 3
-PYTHON_VERSION=3.9.0
-echo "Installing pyenv & python 3..."
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-exec "$SHELL"
-pyenv install $PYTHON_VERSION
-pip install -U pip
-echo "Done" && echo
+# Python & pip 3
+execute_command "git clone https://github.com/pyenv/pyenv.git ~/.pyenv" "cloning pyenv project"
+execute_command "echo 'export PYENV_ROOT=\"$HOME/.pyenv\"' >> ~/.bashrc && echo 'export PATH=\"$PYENV_ROOT/bin:$PATH\"' >> ~/.bashrc" "updating .bashrc"
+execute_command "echo -e "if command -v pyenv 1>/dev/null 2>&1; then\n eval \"$(pyenv init -)\"\nfi" >> ~/.bashrc" "updating .bashrc again"
+execute_command "exec '$SHELL' && pyenv install $PYTHON_VERSION" "installing python 3"
+execute_command "pip install -U pip" "upgrading pip"
 
-## Zooplus
 ### Teams, Slack....
 ## SDKMan
 ## Terraform
@@ -49,8 +50,8 @@ echo "Done" && echo
 #TFSEC_VERSION="0.13.0"
 #wget https://github.com/liamg/tfsec/releases/download/v$TFSEC_VERSION/tfsec-linux-amd64
 
-## Cleanup
-apt -qq autoremove -y && apt -qq autoclean -y && apt -qq upgrade -y
+# Cleanup
+execute_command "apt full-upgrade -y" "cleaning up not used packages, indexes, etc"
 
 # Reboot the system for some changes to have effect
 shutdown -r 1 "The system is going to be restarted in one minute!! Run shutdown -c to cancel it"
